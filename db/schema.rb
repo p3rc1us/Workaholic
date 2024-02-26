5178ba30c6e048d07b3c799fc21cd9bfe64be092
p3rc1us <kervymaniego@gmail.com>
2024-02-26 14:32:57 +0800

    added completed column in tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_094116) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_063139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_094116) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
