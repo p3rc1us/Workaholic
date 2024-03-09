b879c06639d93f57909364d2386a40b8296e6d3a
p3rc1us <kervymaniego@gmail.com>
2024-03-09 15:07:24 +0800

    edited controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_category, only: %i[ show edit update destroy ]
 
   def index
-    if user_signed_in?
-      @categories = current_user.categories
-      @tasks_for_today = current_user.tasks.where(created_at: Date.current.beginning_of_day..Date.current.end_of_day)
-    else
-      redirect_to new_user_session_path
-    end
-    puts @tasks_for_today.count
+    @categories = current_user.categories
+    @tasks_for_today = current_user.tasks.where(created_at: Date.current.beginning_of_day..Date.current.end_of_day)
   end
 
   def show
@@ -26,38 +20,29 @@ class CategoriesController < ApplicationController
   def create
     @category = current_user.categories.build(category_params)
 
-    respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+        redirect_to category_url(@category), notice: "Category was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
     end
-  end
 
   def update
-
-    respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
+        redirect_to category_url(@category), notice: "Category was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
-
   end
 
   def destroy
     @category.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-    end
+      redirect_to categories_url, notice: "Category was successfully destroyed."
   end
 
   private
   def set_category
-    @category = Category.find(params[:id])
+    @category = current_user.categories.find(params[:id])
   end
 
   def category_params
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,17 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_task, only: %i[ show edit update destroy]
+
   before_action :set_category, only: %i[ new edit create update destroy]
+  before_action :set_task, only: %i[ show edit update destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
 
   def edit
@@ -19,40 +19,39 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = @category.tasks.create(task_params)
-    redirect_to category_path(@category)
+    @task = @category.tasks.build(task_params)
+
+    if @task.save
+      redirect_to category_path(@category)
+    else
+      render :form
+    end
+
   end
 
   def update
     if edit
-      respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to category_path(@category), notice: "Task was successfully updated." }
-
+        redirect_to category_path(@category), notice: "Task was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-
-      end
+        render :edit, status: :unprocessable_entity
       end
     end
   end
 
   def destroy
     @task.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to category_path(@category), notice: "Task was successfully destroyed." }
-    end
+      redirect_to category_path(@category), notice: "Task was successfully destroyed."
   end
 
   private
 
-  def set_task
-    @task =  Task.find(params[:id])
+  def set_category
+    @category = current_user.categories.find(params[:category_id])
   end
 
-  def set_category
-    @category = Category.find(params[:category_id])
+  def set_task
+    @task =  @category.tasks.find(params[:id])
   end
 
   def task_params
