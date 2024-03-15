4d1790ed5d2d033ca23e589b573b9ddbed0f6f57
p3rc1us <kervymaniego@gmail.com>
2024-03-15 10:51:06 +0800

    done with category model validation rspec test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it "can visit categories" do
-    expect(false).to be(false)
+
+  let!(:avion) { Category.new }
+
+  context 'Validations' do
+    it '1. Must be invalid without a name' do
+      avion.name = nil
+
+      expect(avion).to_not be_valid
+      expect(avion.errors).to be_present
+      expect(avion.errors[:name]).to include("can't be blank")
+    end
   end
 end
