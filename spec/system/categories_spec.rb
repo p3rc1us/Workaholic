2300704c9cf6466ba47063054c745d387a4845f8
p3rc1us <kervymaniego@gmail.com>
2024-03-15 10:16:08 +0800

    added config in rails helper for devise rspec system

--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :system do
+  # let(:user) { create(:user) }
+
   before do
-    # driven_by(:selenium, using: :chrome, screen_size: [1400, 1400])
-    driven_by :selenium, using: :headless_chrome
+    driven_by(:selenium, using: :chrome, screen_size: [1400, 1400])
+    # driven_by :selenium, using: :headless_chrome
+    # driven_by(:rack_test)
   end
 
-  it "can visit homepage" do
+  # before :each do
+  #   sign_in user
+  # end
+
+  it "sign in" do
     visit root_path
     expect(page).to have_content('Workaholic')
   end
