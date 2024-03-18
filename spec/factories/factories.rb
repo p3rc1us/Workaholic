95a2324705ac34363ffd840bada30bf915a622ab
p3rc1us <kervymaniego@gmail.com>
2024-03-18 12:06:19 +0800

    attempt to create category using category factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com" } #email { 'test@example.com' }
     password { "123456" }
   end
+
+  factory :category do
+    category_name { "New Category from rspec" }
+  end
 end
--- spec/system/categories_spec.rb
+++ spec/system/categories_spec.rb
@@ -9,15 +9,22 @@ RSpec.describe "User Auth", type: :system do
   # driven_by(:rack_test)
   end
 
-  it "from login user" do
+  it "sign in and create new category" do
     visit root_path
     user = FactoryBot.create(:user)
+    category = FactoryBot.create(:category)
 
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_on "Sign in"
 
     expect(page).to have_content("Categories")
-    expect(page).to have_content("For Today")
+
+    visit new_category_path
+
+
+    fill_in "category_name", with: category.category_name
+    click_on "Create Category"
+    expect(page).to have_content("New Category from rspec")
   end
 end
