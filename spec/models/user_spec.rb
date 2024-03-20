3d969b7c15484ef23b526d68964b2c9feead8681
p3rc1us <kervymaniego@gmail.com>
2024-03-20 14:13:18 +0800

    user spec test has many tasks through categories check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe User, type: :model do
     it "have many categories" do
       expect(User.reflect_on_association(:categories).macro).to eq(:has_many)
     end
+
+    it "have many tasks through categories" do
+      expect(User.reflect_on_association(:tasks).macro).to eq(:has_many)
+      expect(User.reflect_on_association(:tasks).through_reflection.name).to eq(:categories)
+    end
   end
 end
