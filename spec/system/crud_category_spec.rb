37c2c051a63b1a5c8e2aaf7369f1a76be4c804b0
p3rc1us <kervymaniego@gmail.com>
2024-03-25 09:52:35 +0800

    crud category sign_in abstracted, it updates done, delete next

--- spec/system/crud_category_spec.rb
+++ spec/system/crud_category_spec.rb
@@ -2,29 +2,54 @@ require "rails_helper"
 
 RSpec.describe "CRUD category", type: :system do
   include Devise::Test::IntegrationHelpers
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:cat) { FactoryBot.create(:category, user: user) }
+
   before do
-  # by default we are using GUI
+  # by default we are using GUI, but it won't work if sign_in is abstracted.
+  # this test is meant for headless & rack only.
   driven_by :selenium, using: :headless_chrome
   # driven_by(:rack_test)
+  sign_in
   end
 
-  it "sign in and create category" do
-    visit root_path
-    user = FactoryBot.create(:user)
+  context "CRUD category" do
 
+    it "create category" do
+      visit new_category_path
+      expect(page).to have_content("New Category")
+
+      fill_in "category_name", with: cat.name
+      click_on "Create Category"
+      expect(page).to have_content(cat.name)
+    end
+
+    it "update category" do
+      visit new_category_path
+      expect(page).to have_content("New Category")
+
+      fill_in "category_name", with: cat.name
+      click_on "Create Category"
+      expect(page).to have_content(cat.name)
+
+      visit edit_category_path(cat)
+      expect(page).to have_content("New Category")
+      fill_in "category_name", with: "#{cat.name} update"
+      click_on "Update Category"
+      expect(page).to have_content("#{cat.name} update")
+    end
+  end
+
+
+
+  private
+
+  def sign_in
+    visit root_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_on "Sign in"
     expect(page).to have_content("Categories")
-
-    visit new_category_path
-    expect(page).to have_content("New Category")
-    cat = FactoryBot.create(:category, user: user)
-
-    fill_in "category_name", with: cat.name
-    click_on "Create Category"
-    expect(page).to have_content(cat.name)
-
-    click_on "Logout"
   end
 end
